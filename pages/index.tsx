24e70e29c32a3524136bd22b2b7b4092f3140100
Nam Tran <tranchinamit@gmail.com>
2022-08-14 17:58:54 +0700

    added spinner

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Intro from '@components/Intro';
 import Pricing from '@components/Pricing';
 import Related from '@components/Related';
 import { IUserData, IManga, MANGA_MOCK_DATA, USER_MOCK_DATA } from '@mocks/data';
+import { Spin } from 'antd';
 
 const DataContext = createContext<{ manga: IManga, user: IUserData } | undefined>(undefined);
 export const useData = (): { manga: IManga, user: IUserData } | undefined => useContext(DataContext);
@@ -20,13 +21,20 @@ export default function Home() {
   const [user, setUser] = useState<IUserData | undefined>(undefined);
   const [manga, setManga] = useState<IManga | undefined>(undefined);
 
+  const [isFetch, setFetch] = useState<boolean>(false);
+
   useEffect(() => {
     let timer = setTimeout(() => {
       setUser(USER_MOCK_DATA ?? undefined);
       setManga(MANGA_MOCK_DATA?.[mangaId] ?? undefined);
+      setFetch(true);
     }, 250);
+
     return () => {
-      clearTimeout(timer)
+      clearTimeout(timer);
+      setUser(undefined)
+      setManga(undefined)
+      setFetch(false)
     };
   }, []);
 
@@ -39,18 +47,24 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <Layouts
-          leftContent={<>
-            <Intro />
-            <Pricing />
-            <Chapters />
-          </>}
-          rightContent={<>
-            <About />
-            <Related />
-            <Comments />
-          </>}
-        />
+        {isFetch ? (
+          <Layouts
+            leftContent={<>
+              <Intro />
+              <Pricing />
+              <Chapters />
+            </>}
+            rightContent={<>
+              <About />
+              <Related />
+              <Comments />
+            </>}
+          />
+        ) : (
+          <div className="wrap-spin">
+            <Spin />
+          </div>
+        )}
 
         <footer />
       </div>
