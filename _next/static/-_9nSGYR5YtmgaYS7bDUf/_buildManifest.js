72ef218c7aa374422bd044d4b52c1b4e2d008edb
tranchinamit <48767069+tranchinamit@users.noreply.github.com>
2023-12-21 04:14:30 +0000

    Deploying to gh-pages from @ tranchinamit/manga@e5e72c9314f920cd1bbd6d09ad851fbe484562ac 🚀

--- _next/static/-_9nSGYR5YtmgaYS7bDUf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/330-7a4bbf5cd1bcd28b.js","static/chunks/pages/index-e171d8ec71b6214f.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
