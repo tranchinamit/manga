2c0ff5afce02a3dc0d32b98710a0962c9652a668
NamTran <nam.tranchi@saigontechnology.com>
2023-12-27 13:31:51 +0700

    testing blocking new tab

--- src/components/GetApp/index.tsx
+++ src/components/GetApp/index.tsx
@@ -2,12 +2,15 @@ import { Image, Typography, Card, Button } from "antd";
 
 const { Text } = Typography;
 
-const githubUrl  ="https://github.com/tranchinamit";
+const githubUrl = "https://github.com/tranchinamit";
 
 export default function GetApp() {
-  const handleOpenNewTab = () => {
-    window.open(githubUrl, "_blank");
-  }
+  const handleOpenNewTab = async () => {
+    const res = await (await fetch("https://dummyjson.com/todos")).json();
+    console.log(res.todos[0].todo);
+    const firstTodo = res.todos[0].todo;
+    window.open(`${githubUrl}?todo=${firstTodo}`, "_blank");
+  };
 
   return (
     <Card className="block">
@@ -20,8 +23,9 @@ export default function GetApp() {
               devices.
             </Text>
           </div>
-          <a href={githubUrl} target="_blank">Visit my Github!</a>
-          <Button onClick={handleOpenNewTab} type="text" danger>Visit my Github!</Button>
+          <Button onClick={handleOpenNewTab} type="text" danger>
+            Visit my Github!
+          </Button>
         </div>
         <div>
           <Image width={116} src="images/icon/qr.png" preview={false} alt="qr" />
